97b57c83cbce2fa3be318231eb53b4af395578cb
LooserRIP <46068464+LooserRIP@users.noreply.github.com>
2022-04-10 23:15:38 +0300

    MIKE PENIS ADDED

--- shadle/script.js
+++ shadle/script.js
@@ -8,7 +8,9 @@ let colorgoalbright = 0;
 let colorgoal = "#000000";
 let guesses = [];
 let won = false;
+let gamedone = false;
 let mouseDown = [0,0,0,0,0,0,0,0];
+let ismobile = false;
 
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -25,6 +27,12 @@ function init() {
   document.body.onmouseup = function(evt) {
     mouseDown[evt.button] = false;
   }
+  if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+    ismobile = true;
+    addcss("shadle/mobilestyle.css");
+  }
+
 }
 
 function updateStreak() {
@@ -76,7 +84,7 @@ function clickcolor(elm) {
 }
 
 function consolelog() {
-  
+
 }
 
 
@@ -113,6 +121,7 @@ function generateColor() {
 
 
 function submitcolor(firsttime) {
+  if (gamedone) return;
   if (guesses.length >= 4) return;
   guesses.push({h: colorhue, s: colorsatur, v: colorbright});
   /*
@@ -127,9 +136,11 @@ function submitcolor(firsttime) {
   var firstTimeWin = firsttime;
   localStorage.shadle_guesses = JSON.stringify(guesses);
   if (dist == 100) {
+    gamedone = true;
     win(firstTimeWin);
   } else {
     if (guesses.length == 4) {
+      gamedone = true;
       defeat(firstTimeWin);
     }
   }
@@ -166,7 +177,7 @@ async function win(first) {
   await sleep(1400);
   document.getElementById("ws_played").innerText = JSON.parse(localStorage.shadle_stats).w + JSON.parse(localStorage.shadle_stats).l;
   document.getElementById("ws_str").innerText = JSON.parse(localStorage.shadle_stats).str;
-  document.getElementById("ws_mstr").innerText = JSON.parse(localStorage.shadle_stats).mstr;
+  //document.getElementById("ws_mstr").innerText = JSON.parse(localStorage.shadle_stats).mstr;
   document.getElementById("ws_wlr").innerText = Math.floor(JSON.parse(localStorage.shadle_stats).w / (JSON.parse(localStorage.shadle_stats).w + JSON.parse(localStorage.shadle_stats).l) * 100) + "%";
   document.getElementById("winui").dataset.reveal = "1";
 }
@@ -184,7 +195,7 @@ async function defeat(first) {
   await sleep(1400);
   document.getElementById("ws_played").innerText = JSON.parse(localStorage.shadle_stats).w + JSON.parse(localStorage.shadle_stats).l;
   document.getElementById("ws_str").innerText = JSON.parse(localStorage.shadle_stats).str;
-  document.getElementById("ws_mstr").innerText = JSON.parse(localStorage.shadle_stats).mstr;
+  //document.getElementById("ws_mstr").innerText = JSON.parse(localStorage.shadle_stats).mstr;
   document.getElementById("ws_wlr").innerText = Math.floor(JSON.parse(localStorage.shadle_stats).w / (JSON.parse(localStorage.shadle_stats).w + JSON.parse(localStorage.shadle_stats).l) * 100) + "%";
   document.getElementById("wintext").innerText = "LLLLL";
   document.getElementById("winui").dataset.reveal = "1";
@@ -248,6 +259,7 @@ async function createGuess(h, s, v, dist, ind) {
   document.getElementById("guesses").appendChild(guess);
 
   var diff = Math.abs(Math.round(h / 36) - Math.round(colorgoalhue / 36));
+  if (colorgoalsatur == 0 || colorgoalbright == 0) diff = 0;
   if (diff == 0) {
     guesshinthue.dataset["type"] = "correct";
   } else if (diff == 1 || diff == 9) {
@@ -256,6 +268,7 @@ async function createGuess(h, s, v, dist, ind) {
     guesshinthue.dataset["type"] = "incorrect";
   }
   var diff2 = Math.abs(Math.round(s / 0.1) - Math.round(colorgoalsatur / 0.1));
+  if (colorgoalbright == 0) diff2 = 0;
   if (diff2 == 0) {
     guesshintsatur.dataset["type"] = "correct";
   } else if (diff2 == 1) {
@@ -349,6 +362,12 @@ function rgbToHex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
+async function closemenu() {
+  document.getElementById("winui").dataset.reveal = "0";
+  await sleep(500);
+  document.getElementById("winuioverlay").dataset.reveal = "0";
+}
+
 function share() {
 
   var now = new Date();
@@ -364,10 +383,12 @@ function share() {
     var charsatur = "⚫";
     var charbright = "⚫";
     var diff = Math.abs(Math.round(guess.h / 36) - Math.round(colorgoalhue /36));
+    if (colorgoalsatur == 0 || colorgoalbright == 0) diff = 0;
     if (diff == 0) {
       charhue = "🟢";
     } else if (diff == 1 || diff == 9) {charhue = "🟡"}
     var diff = Math.abs(Math.round(guess.s / 0.1) - Math.round(colorgoalsatur /0.1));
+    if (colorgoalbright == 0) diff = 0;
     if (diff == 0) {
       charsatur = "🟢";
     } else if (diff == 1) {charsatur = "🟡"}
