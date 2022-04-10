f1c55d6789d9d067b4236eadbdbaaab35c4ce985
LooserRIP <46068464+LooserRIP@users.noreply.github.com>
2022-04-10 23:28:00 +0300

    ADKWOADKWPOAKDOPWKDPOWADWA

--- shadle/script.js
+++ shadle/script.js
@@ -404,3 +404,12 @@ function share() {
 
   navigator.clipboard.writeText(sharetext);
 }
+
+
+function addcss(path) {
+  var fileref = document.createElement("link");
+  fileref.setAttribute("rel", "stylesheet");
+  fileref.setAttribute("type", "text/css");
+  fileref.setAttribute("href", path);
+  document.getElementsByTagName("head")[0].appendChild(fileref);
+}
