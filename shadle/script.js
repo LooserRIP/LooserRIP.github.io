459e857f97c56a687fc17b7042b8e386a5a70a2a
LooserRIP <46068464+LooserRIP@users.noreply.github.com>
2022-04-11 14:50:43 +0300

    CUM BITCH FUCK ASS OMG

--- shadle/script.js
+++ shadle/script.js
@@ -506,33 +506,6 @@ function settweet() {
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+  
 });
 
-async function getapp() {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-  });
-}
-
-function addcss(path) {
-  var fileref = document.createElement("link");
-  fileref.setAttribute("rel", "stylesheet");
-  fileref.setAttribute("type", "text/css");
-  fileref.setAttribute("href", path);
-  document.getElementsByTagName("head")[0].appendChild(fileref);
-}
