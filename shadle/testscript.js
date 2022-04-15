ba40ea6734d891b19e79229e2b20573770f16a8e
LooserRIP <46068464+LooserRIP@users.noreply.github.com>
2022-04-16 00:18:12 +0300

    a

--- shadle/testscript.js
+++
@@ -1,7 +0,0 @@
-function init() {
-  var stats = JSON.parse(localStorage["shadle_stats"]);
-  stats.str = 8;
-  stats.l = 0;
-  stats.w = 8;
-  localStorage["shadle_stats"] = JSON.stringify(stats);
-}
