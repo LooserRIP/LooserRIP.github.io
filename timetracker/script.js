7097a4f80a29a53fb947cb744e48a59529248ac3
LooserRIP <korenandkoren@gmail.com>
2022-09-21 13:06:00 +0300

    אקדא

---
+++ timetracker/script.js
@@ -0,0 +1,30 @@
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+} //
+
+function init() {
+  initLocalStorage();
+  refreshTimeButtons();
+}
+function initLocalStorage() {
+  if (localStorage["tt_types"] == undefined) localStorage["tt_types"] = JSON.stringify(["School", "Work", "Sleep"]);
+
+}
+
+function refreshTimeButtons() {
+  let types = JSON.parse(localStorage.tt_types);
+
+}
+function createTimeButton() {
+  
+}
+
+
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log("PROMPTTTT");
+  e.prompt();
+});
+
