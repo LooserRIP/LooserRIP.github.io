4af58f93fb8db2d84977bc83295da3736b192155
LooserRIP <korenandkoren@gmail.com>
2023-06-04 12:06:56 +0300

    df bukkit changes

--- dfbukkit/script.js
+++ dfbukkit/script.js
@@ -1,3 +1,27 @@
+let actiondump;
+function init() {
+    fetch("https://raw.githubusercontent.com/LooserRIP/LooserRIP.github.io/master/dfbukkit/action_dump.json")
+    .then(res => res.json())
+    .then(data => actiondump = data)
+}
+
+function findAction(actionname) {
+    if (actiondump == undefined) return 0;
+    let findID = 0;
+    let findVal = 0;
+    let idRun = 0;
+    for (const runthrough of actiondump.actions) {
+        let jarodist = jaroWinkler(runthrough.name.toLowerCase(), actionname.toLowerCase());
+        if (jarodist >= findVal) {
+            findVal = jarodist;
+            findID = idRun;
+        }
+        idRun++;
+    }
+    console.log(actiondump.actions[findID].name, actionname, findVal);
+    return findID;
+}
+
 function test() {
     const templateList = [];
     let splitinput = document.getElementById("w3review").value.split("\n");
@@ -15,6 +39,7 @@ function test() {
         let jsonData = JSON.parse(strData);
         console.log(jsonData.blocks);
         let i = 0;
+        let spacesCount = 0;
         for (const block of jsonData.blocks) {
             if (block.id == "block") {
                 console.log(block.block, block.action, block.id);
@@ -23,26 +48,50 @@ function test() {
                     if (block.data != undefined) {
                         dataname = block.data
                     } else {
-                        dataname = formatString(block.action.replace("_", " "));
+                        dataname = block.action.replace("_", " ");
                     }
                     let nameOfEvent = titleCase(block.block.replace("_", " ")).replace(" ", "");
                     if (nameOfEvent == "Event") nameOfEvent = "PlayerEvent";
                     if (nameOfEvent == "Process") nameOfEvent = "AsyncFunction";
-                    finalString = finalString + nameOfEvent + "." + dataname.replace(" ", "") + " {"
+                    finalString = finalString + nameOfEvent + "." + dataname.replace(" ", "-") + " {\n"
+                    spacesCount++;
                 }
                 if (i > 0) {
                     let nameOfBlock = titleCase(block.block.replace("_", " ")).replace(" ", "");
-                    let nameOfAction = titleCase(block.action.replace("_", " ")).replace(" ", "");
-                    finalString = finalString + nameOfBlock + "." + nameOfAction;
-                    if (i != jsonData.blocks.length - 1) {
-                        finalString = finalString + ", "
+                    let nameOfAction = block.action;
+                    if (spacesCount > 0) {
+                        finalString += " ".repeat(spacesCount * 4);
+                    }
+                    finalString = finalString + nameOfBlock + "." + nameOfAction + "(";
+                    args = [];
+                    for (const argument of actiondump.actions[findAction(nameOfAction)].icon.arguments) {
+                        console.log(argument);
+                        args.push(argument.description + ": ");
+                    }
+                    finalString += args.join(", ")
+                    finalString = finalString + ")";
+                    if (!(i != jsonData.blocks.length - 1 && jsonData.blocks[i + 1].id == "bracket")) {
+                        finalString = finalString + ";\n"
+                    } else {
+                        finalString = finalString + " ";
                     }
                 }
             } else if (block.id == "bracket") {
-
+                if (block.direct == "open") {
+                    //finalString += " ".repeat(spacesCount * 4);
+                    finalString = finalString += "{\n"
+                    spacesCount++;
+                }
+                if (block.direct == "close") {
+                    spacesCount--;
+                    finalString = finalString + "\n";
+                    finalString += " ".repeat(spacesCount * 4);
+                    finalString = finalString + "}\n"
+                }
             }
             i++;
         }
+        finalString = finalString + "}";
         finalStrings.push(finalString);
         console.warn(finalString);
     })
@@ -72,3 +121,60 @@ function titleCase(str) {
   }
   
   
+
+
+  
+function jaroWinkler(s1, s2) {
+    let jaroDist = jaroDistance(s1, s2);
+  
+    if (jaroDist > 0.7) {
+      let prefix = 0;
+  
+      for (let i = 0; i < Math.min(s1.length, s2.length); i++) {
+        if (s1[i] === s2[i]) prefix++;
+        else break;
+      }
+  
+      prefix = Math.min(4, prefix);
+      jaroDist += 0.1 * prefix * (1 - jaroDist);
+    }
+  
+    return jaroDist;
+  }
+  
+  
+function jaroDistance(s1, s2) {
+    if (s1 === s2) return 1.0;
+  
+    let len1 = s1.length, len2 = s2.length;
+    let maxDist = Math.floor(Math.max(len1, len2) / 2) - 1;
+    let match = 0;
+    let hashS1 = new Array(s1.length).fill(0);
+    let hashS2 = new Array(s2.length).fill(0);
+  
+    for (let i = 0; i < len1; i++) {
+      for (let j = Math.max(0, i - maxDist); j < Math.min(len2, i + maxDist + 1); j++) {
+        if (s1[i] === s2[j] && hashS2[j] === 0) {
+          hashS1[i] = 1;
+          hashS2[j] = 1;
+          match++;
+          break;
+        }
+      }
+    }
+  
+    if (match === 0) return 0.0;
+  
+    let t = 0;
+    let point = 0;
+  
+    for (let i = 0; i < len1; i++) {
+      if (hashS1[i] === 1) {
+        while (hashS2[point] === 0) point++;
+        if (s1[i] !== s2[point++]) t++;
+      }
+    }
+  
+    t /= 2;
+    return ((match) / (len1) + (match) / (len2) + (match - t) / (match)) / 3.0;
+  }
