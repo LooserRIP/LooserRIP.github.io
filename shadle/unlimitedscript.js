d73d886d2e386fc1ccb4e4254bb51a2d49ea450e
LooserRIP <looserdeveloper@gmail.com>
2024-08-14 18:36:05 +0300

    moved projects to their separate repositories.

--- shadle/unlimitedscript.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    if (localStorage["shadle_unlimited"] == "1") {
-        localStorage["shadle_unlimited"] = "0";
-        document.getElementById("toggle").innerText = "Unlimited mode toggled OFF.";
-    } else {
-        localStorage["shadle_unlimited"] = "1";
-        document.getElementById("toggle").innerText = "Unlimited mode toggled ON.";
-    }
-}
